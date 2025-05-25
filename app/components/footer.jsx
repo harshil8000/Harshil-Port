cdd5680a1a1758c8cf2db9ca62bd72c967025052
Harshil Barad <90130954+harshil8000@users.noreply.github.com>
2025-05-25 18:45:46 +0530

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
 
+
 function Footer() {
   return (
     <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
