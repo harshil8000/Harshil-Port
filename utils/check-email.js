be4be34d0c25d9af09ea91f902f8ac4c019ad0e4
Harshil Barad <90130954+harshil8000@users.noreply.github.com>
2025-08-01 14:24:54 +0530

    Update check-email.js

--- utils/check-email.js
+++ utils/check-email.js
@@ -2,6 +2,7 @@ export function isValidEmail(email) {
   // Regular expression for validating email addresses
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
+
 };
 
 
